00ba3b5ae056c8ff4b7831c334b6b95b1e75a219
Adam Davies <Mrxblacksmith@gmail.com>
2019-08-16 18:34:45 +0000

    setup master

---
+++ master.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Project0
+{
+    public class Novel
+    {
+            private int pages;
+            public int Pages
+            {
+                get
+                {
+                    return pages;
+                }
+                set
+                {
+                    pages = value;
+                }
+
+            }
+            // properities was added at a later time where we can do somthing?
+            //
+            public Novel()
+            {
+                this.pages = 0;
+            }
+            public Novel(int pages ) //: this(ID1,lastName1, firstName1)
+            {
+                this.pages = pages;
+                //
+            }
+    }
+}

9102d098c612167672ee773c58becb68f3a6ebe1
dfoor1142 <54186802+dfoor1142@users.noreply.github.com>
2019-08-16 14:37:12 -0400

    Add files via upload

---
+++ group assignment 0 creator class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Assignment_0
+{
+    
+    class Creator
+    {
+        private String firstName;
+        public String FirstName;
+        
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        private String lastName;
+        public String LastName;
+        {
+        get
+        {    
+                return lastName;
+        }   
+            set
+        {
+                lastName = value;
+        }
+        }        
+}

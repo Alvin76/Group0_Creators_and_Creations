1734cd9b1baa74cde8827c82a39beff47eb58082
Adam Davies <45388618+Alvin76@users.noreply.github.com>
2019-08-16 14:47:35 -0400

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -80,4 +80,42 @@ namespace Group_Assignment_0
             }
     }
 //}
+  
+
+
+   class Painting : Creation
+    {
+        private int width;
+        private int height;
+
+        public Painting(int Width, int Height)
+        {
+            this.width = Width;
+            this.height = Height;
+        }
+        
+        public int width()
+        {   
+               get 
+               {
+               return width;
+               }
+               set 
+               {
+               width = value;
+               }
+        }
+
+        public int height()
+        {
+            get 
+            {
+            return height;
+            }
+            set
+            {
+            height = value;
+            }
+        }
+    }
 }

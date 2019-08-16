a6c05f39b855550e155eac5923baa51303cba8fa
WilsonMarquis <54186796+WilsonMarquis@users.noreply.github.com>
2019-08-16 14:44:31 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    interface Creation {
+        string author {
+            get{
+                return "Author: " + firstName + " " + lastName;
+            }
+        }
+
+        string type{
+            get{
+                if (type = "novel") {
+                    return "Novel with " + this.numOfPages + "pages.";
+                }
+
+                return type;
+                }
+        }
+        string title{
+            get{
+                return "Title: " + title;
+                }
+            }
+
+}
+}
